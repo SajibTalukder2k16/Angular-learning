48c348ec799bf8c3ef3d875ff5443dd341bf70fe
SajibTalukder2k16 <sajibtalukder2k16@gmail.com>
2021-10-24 17:42:27 +0600

    error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe_list/recipe-item/recipe-item.component';
+
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
