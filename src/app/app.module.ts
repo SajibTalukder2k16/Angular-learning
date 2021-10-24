e2578d16b3aca6c6407b5d934468b3c483b5850a
SajibTalukder2k16 <sajibtalukder2k16@gmail.com>
2021-10-24 17:38:22 +0600

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 
 import {FormsModule} from "@angular/forms";
 import {serverComponent} from "./server/server.component";
+import { HeaderComponent } from 'src/app/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe_list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    serverComponent
+    serverComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,9 +6,18 @@ import {Component} from "@angular/core";
 })
 export  class serverComponent{
   makeEnable = false;
+  serverComponentStatus = "No server is not created";
+  serverName='';
+  servers =['1','2','3'];
   constructor() {
     setTimeout(()=>{
       this.makeEnable=true;
     },2000)
+  }
+  onCreateServer(){
+    this.serverComponentStatus= "Server Created";
+  }
+  onUpdateServer(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
