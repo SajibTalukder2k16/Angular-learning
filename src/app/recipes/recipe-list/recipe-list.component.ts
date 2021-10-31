e1c493241613e9f947ba4c6f9a47d5b2e62121c9
SajibTalukder2k16 <sajibtalukder2k16@gmail.com>
2021-10-31 17:34:15 +0600

    model added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   check="Custom Pipe checking";
+  recipes:Recipe[]=[
+    new Recipe("Burger","Delicious","https://insanelygoodrecipes.com/wp-content/uploads/2020/05/Burger-with-fries.webp"),
+    new Recipe("Burger","Delicious","https://insanelygoodrecipes.com/wp-content/uploads/2020/05/Burger-with-fries.webp")
+  ];
   constructor() { }
 
   ngOnInit(): void {
